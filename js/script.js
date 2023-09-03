88239a75ad4a20abea3702f04dc1acd3c8ad05b2
Paula_Dev <paulasuyaneprojeto2023@gmail.com>
2023-09-03 00:14:39 -0300

    inicialização

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+MensagemName = document.getElementById('name').value
+MensagemEmail = document.getElementById('email').value
+MensagemText = document.getElementById('message').value
+Resposta = document.getElementById('res').value
+
+function EnviarMensagem(){
+
+}
+
